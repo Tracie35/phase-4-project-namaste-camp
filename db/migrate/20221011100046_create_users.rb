6bbdad02045a3c70dba6d61a633f9f0942ef4998
tracy <kariukitracie@gmail.com>
2022-10-11 14:16:52 +0300

    milestone:updated activity table

--- db/migrate/20221011100046_create_users.rb
+++ db/migrate/20221011100046_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.integer :age
-      t.string :password
+      t.string :password_digest
       t.string :email
 
       t.timestamps
